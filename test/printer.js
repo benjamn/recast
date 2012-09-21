3f367ba648b8758c91eab3cd696618492915da7d
Ben Newman <ben@quora.com>
2012-09-21 18:30:05 +0000

    implement printer module, with tests

---
+++ test/printer.js
@@ -0,0 +1,27 @@
+var Printer = require("../lib/printer").Printer,
+    Parser = require("../lib/parser").Parser;
+
+function testPrinter(t, assert) {
+    var code = testPrinter + "",
+        parser = new Parser(code),
+        printer = new Printer(parser);
+
+    assert.strictEqual(typeof printer.print, "function");
+    assert.ok(printer.print(null).isEmpty());
+    assert.ok(printer.print(null, true).isEmpty());
+
+    var printed = printer.printGenerically(parser.getAst()),
+        string = printed.toString();
+
+    assert.ok(string.indexOf("t.finish();") > 0);
+
+    printed = printer.print(parser.getAst(), true);
+    string = printed.toString();
+
+    // TODO
+
+    assert.ok(string.indexOf("// TODO") > 0);
+
+    t.finish();
+};
+exports.testPrinter = testPrinter;
