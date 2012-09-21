3b0d5c564afffc03c09e958a5405e4c8cb6106e7
Ben Newman <ben@quora.com>
2012-09-21 18:32:09 +0000

    implement umbrella test module

---
+++ test/run.js
@@ -0,0 +1,18 @@
+function submodule(id) {
+    var module = require("./" + id),
+        name, value;
+    for (var name in module) {
+        value = module[name];
+        if (/^test/.test(name) &&
+            typeof value === "function")
+            exports["test/" + id + "." + name] = value;
+    }
+}
+
+submodule("lines");
+submodule("patcher");
+submodule("Class");
+submodule("visitor");
+submodule("comments");
+submodule("parser");
+submodule("printer");
