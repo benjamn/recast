ede990240b621794bc6e795580fbb28e5ec40abf
Ben Newman <bn@cs.stanford.edu>
2012-11-27 12:18:16 -0800

    Add require("recast").run{File,String} and support options.writeback.
    This will make the toolchain easier to test.

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
 var fs = require("fs"),
-    path = process.argv[2],
+    normalizeOptions = require("./lib/options").normalize,
     Parser = require("./lib/parser").Parser,
     Printer = require("./lib/printer").Printer;
 
-exports.run = function(transformer, options) {
-    fs.readFile(path, "utf-8", function(err, data) {
+function run(transformer, options) {
+    return runFile(process.argv[2], transformer, options);
+}
+
+function runFile(path, transformer, options) {
+    fs.readFile(path, "utf-8", function(err, code) {
         if (err) {
-            console.log(err);
+            console.error(err);
             return;
         }
 
-        var parser = new Parser(data, options),
-            printer = new Printer(parser, options);
-
-        transformer(parser.getAst(), function(node) {
-            var lines = printer.print(node, true);
-            process.stdout.write(lines.toString(options));
-        });
+        runString(code, transformer, options);
     });
-};
+}
+
+function runString(code, transformer, options) {
+    options = normalizeOptions(options);
+
+    var parser = new Parser(code, options),
+        printer = new Printer(parser, options);
+
+    transformer(parser.getAst(), function(node) {
+        var lines = printer.print(node, true);
+        options.writeback(lines.toString(options));
+    });
+}
+
+exports.run = run;
+exports.runFile = runFile;
+exports.runString = runString;
 
 // Useful utilities for implementing transformer functions.
 exports.Syntax = require("./lib/syntax");
