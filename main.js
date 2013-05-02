ba017a7b27c8d345e1efb91f09e04f3fcd8bb92e
Ben Newman <bn@cs.stanford.edu>
2013-05-02 11:16:10 -0400

    Expose require("recast").{namedTypes,builders} and reorganize main.js.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var fs = require("fs");
 var normalizeOptions = require("./lib/options").normalize;
+var types = require("./lib/types");
 var Parser = require("./lib/parser").Parser;
 var Printer = require("./lib/printer").Printer;
 var genericPrinter = new Printer(new Parser(""));
@@ -31,16 +32,60 @@ function runString(code, transformer, options) {
     });
 }
 
-exports.run = run;
-exports.runFile = runFile;
-exports.runString = runString;
+Object.defineProperties(exports, {
+    /**
+     * Scriptable interface to recast.
+     */
+    run: {
+        enumerable: true,
+        value: run
+    },
 
-// Useful utilities for implementing transformer functions.
-exports.Syntax = require("./lib/syntax");
-exports.Visitor = require("./lib/visitor").Visitor;
-exports.builder = require("./lib/types").builders;
+    runFile: {
+        enumerable: true,
+        value: runFile
+    },
 
-// Quick shortcut to the generic pretty-printer.
-exports.print = function(node) {
-    return genericPrinter.printGenerically(node).toString();
-};
+    runString: {
+        enumerable: true,
+        value: runString
+    },
+
+    /**
+     * Useful utilities for implementing transformer functions.
+     */
+    Syntax: {
+        enumerable: true,
+        value: require("./lib/syntax")
+    },
+
+    Visitor: {
+        enumerable: true,
+        value: require("./lib/visitor").Visitor
+    },
+
+    builder: { // Legacy singular form.
+        enumerable: false,
+        value: types.builders
+    },
+
+    builders: { // Plural preferred.
+        enumerable: true,
+        value: types.builders
+    },
+
+    namedTypes: {
+        enumerable: true,
+        value: types.namedTypes
+    },
+
+    /**
+     * Quick shortcut to the generic pretty-printer.
+     */
+    print: {
+        enumerable: true,
+        value: function(node) {
+            return genericPrinter.printGenerically(node).toString();
+        }
+    }
+});
