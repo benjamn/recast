7778a56f274c147f15038bb08811bcbffd697e92
Ben Newman <bn@cs.stanford.edu>
2013-12-01 11:43:51 -0600

    Produce a source map when options.sourceMapName is defined.
    Note: for the source map to be non-empty, the syntax tree must have been
    created by `recast.parse(source, { sourceFileName: "something.js" })`.
    
    Example usage:
    
        var result = recast.print(transform(recast.parse(source, {
            sourceFileName: "source.js"
        })), {
            sourceMapName: "map.json"
        });
    
        console.log(result.code); // Resulting string of code.
        console.log(result.map); // JSON source map.
    
    Closes #43 (yay!).

--- test/mapping.js
+++ test/mapping.js
@@ -1,22 +1,79 @@
+var sourceMap = require("source-map");
+var types = require("ast-types");
+var n = types.namedTypes;
+var NodePath = types.NodePath;
 var fromString = require("../lib/lines").fromString;
 var parse = require("../lib/parser").parse;
 var Printer = require("../lib/printer").Printer;
 var Mapping = require("../lib/mapping");
 
 exports.testMapping = function(t, assert) {
-    var ast = parse([
+    var code = [
         "function foo(bar) {",
         "  return 1 + bar;",
         "}"
-    ].join("\n"), {
+    ].join("\n");
+
+    var lines = fromString(code);
+    var ast = parse(code, {
         sourceFileName: "source.js"
     });
 
-    var printer = new Printer({
-        sourceMapName: "map.js"
-    });
+    var path = new NodePath(ast);
+    var returnPath = path.get("program", "body", 0, "body", "body", 0);
+    n.ReturnStatement.assert(returnPath.value);
 
-    console.log(printer.print(ast));
+    var leftPath = returnPath.get("argument", "left");
+    var leftValue = leftPath.value;
+    var rightPath = returnPath.get("argument", "right");
+
+    leftPath.replace(rightPath.value);
+    rightPath.replace(leftValue);
+
+    var printed = new Printer({
+        sourceMapName: "source.map.json"
+    }).print(ast);
+
+    assert.ok(printed.map);
+
+    assert.strictEqual(
+        printed.map.file,
+        "source.map.json"
+    );
+
+    var smc = new sourceMap.SourceMapConsumer(printed.map);
+
+    function check(origLine, origCol, genLine, genCol) {
+        assert.deepEqual(smc.originalPositionFor({
+            line: genLine,
+            column: genCol
+        }), {
+            source: "source.js",
+            line: origLine,
+            column: origCol,
+            name: lines.charAt({
+                line: origLine,
+                column: origCol
+            })
+        });
+
+        assert.deepEqual(smc.generatedPositionFor({
+            source: "source.js",
+            line: origLine,
+            column: origCol
+        }), {
+            line: genLine,
+            column: genCol
+        });
+    }
+
+    check(1, 0, 1, 0); // function
+    check(1, 18, 1, 18); // {
+    check(2, 2, 2, 2); // return
+    check(2, 13, 2, 9); // bar
+    check(2, 9, 2, 15); // 1
+    check(2, 16, 2, 16); // ;
+    check(3, 0, 3, 0); // }
 
     t.finish();
 };
