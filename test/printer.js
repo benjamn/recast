benjamn/recast

commit c0320a45235e67a861529ed02369cc1ce3b07c0f

Insert semicolons in more places.

lib/printer.js
@@ -392,6 +392,8 @@ function genericPrintNoParens(path, options, print) {
         if (n.argument)
             parts.push(" ", print(path.get("argument")));
 
+        parts.push(";");
+
         return concat(parts);
 
     case "CallExpression":
@@ -562,6 +564,8 @@ function genericPrintNoParens(path, options, print) {
             parts.push(printed[0]);
         }
 
+        parts.push(";");
+
         return concat(parts);
 
     case "VariableDeclarator":
@@ -660,12 +664,14 @@ function genericPrintNoParens(path, options, print) {
         var parts = ["break"];
         if (n.label)
             parts.push(" ", print(path.get("label")));
+        parts.push(";");
         return concat(parts);
 
     case "ContinueStatement":
         var parts = ["continue"];
         if (n.label)
             parts.push(" ", print(path.get("label")));
+        parts.push(";");
         return concat(parts);
 
     case "LabeledStatement":
@@ -704,7 +710,8 @@ function genericPrintNoParens(path, options, print) {
     case "ThrowStatement":
         return concat([
             "throw ",
-            print(path.get("argument"))
+            print(path.get("argument")),
+            ";"
         ]);
 
     case "SwitchStatement":
@@ -736,7 +743,7 @@ function genericPrintNoParens(path, options, print) {
         return concat(parts);
 
     case "DebuggerStatement":
-        return fromString("debugger");
+        return fromString("debugger;");
 
     // XJS extensions below.
 

test/printer.js
@@ -238,7 +238,16 @@ exports.testSimpleVarPrinting = function(t, assert) {
 
     assert.strictEqual(
         printer.print(varDecl).code,
-        "var x, y, z"
+        "var x, y, z;"
+    );
+
+    var z = varDecl.declarations.pop();
+    varDecl.declarations.pop();
+    varDecl.declarations.push(z);
+
+    assert.strictEqual(
+        printer.print(varDecl).code,
+        "var x, z;"
     );
 
     t.finish();
@@ -262,7 +271,7 @@ exports.testMultiLineVarPrinting = function(t, assert) {
         "    y = {",
         "      why: \"not\"",
         "    },",
-        "    z"
+        "    z;"
     ].join("\n"));
 
     t.finish();

commit c8d313a1e265909d50a9191e6c64f2afd42d4a4e

Fix printing of var decls in loop initializers
See facebook/regenerator#103.

c0320a4 broke this; variable declarators should not always be followed by semicolons (particularly in a for/in loop, where "for (var key; in obj)" is wrong). I also made sure that empty statements are printed correctly in the context of a for loop.

Test Plan:
npm test

lib/printer.js
@@ -564,8 +564,6 @@ function genericPrintNoParens(path, options, print) {
             parts.push(printed[0]);
         }
 
-        parts.push(";");
-
         return concat(parts);
 
     case "VariableDeclarator":
@@ -1027,7 +1025,7 @@ function nodeStr(n) {
 
 function maybeAddSemicolon(lines) {
     var eoc = lastNonSpaceCharacter(lines);
-    if (eoc && "\n};".indexOf(eoc) < 0)
+    if (!eoc || "\n};".indexOf(eoc) < 0)
         return concat([lines, ";"]);
     return lines;
 }

test/printer.js
@@ -237,7 +237,7 @@ exports.testSimpleVarPrinting = function(t, assert) {
     ]);
 
     assert.strictEqual(
-        printer.print(varDecl).code,
+        printer.print(b.program([varDecl])).code,
         "var x, y, z;"
     );
 
@@ -246,7 +246,7 @@ exports.testSimpleVarPrinting = function(t, assert) {
     varDecl.declarations.push(z);
 
     assert.strictEqual(
-        printer.print(varDecl).code,
+        printer.print(b.program([varDecl])).code,
         "var x, z;"
     );
 
@@ -266,13 +266,77 @@ exports.testMultiLineVarPrinting = function(t, assert) {
         b.variableDeclarator(b.identifier("z"), null)
     ]);
 
-    assert.strictEqual(printer.print(varDecl).code, [
+    assert.strictEqual(printer.print(b.program([varDecl])).code, [
         "var x,",
         "    y = {",
         "      why: \"not\"",
         "    },",
         "    z;"
     ].join("\n"));
+
+    t.finish();
+};
+
+exports.testForLoopPrinting = function(t, assert) {
+    var printer = new Printer({ tabWidth: 2 });
+    var loop = b.forStatement(
+        b.variableDeclaration("var", [
+            b.variableDeclarator(b.identifier("i"), b.literal(0))
+        ]),
+        b.binaryExpression("<", b.identifier("i"), b.literal(3)),
+        b.updateExpression("++", b.identifier("i"), /* prefix: */ false),
+        b.expressionStatement(
+            b.callExpression(b.identifier("log"), [b.identifier("i")])
+        )
+    );
+
+    assert.strictEqual(
+        printer.print(loop).code,
+        "for (var i = 0; i < 3; i++)\n" +
+        "  log(i);"
+    );
+
+    t.finish();
+};
+
+exports.testEmptyForLoopPrinting = function(t, assert) {
+    var printer = new Printer({ tabWidth: 2 });
+    var loop = b.forStatement(
+        b.variableDeclaration("var", [
+            b.variableDeclarator(b.identifier("i"), b.literal(0))
+        ]),
+        b.binaryExpression("<", b.identifier("i"), b.literal(3)),
+        b.updateExpression("++", b.identifier("i"), /* prefix: */ false),
+        b.emptyStatement()
+    );
+
+    assert.strictEqual(
+        printer.print(loop).code,
+        "for (var i = 0; i < 3; i++)\n" +
+        "  ;"
+    );
+
+    t.finish();
+};
+
+exports.testForInLoopPrinting = function(t, assert) {
+    var printer = new Printer({ tabWidth: 2 });
+    var loop = b.forInStatement(
+        b.variableDeclaration("var", [
+            b.variableDeclarator(b.identifier("key"), null)
+        ]),
+        b.identifier("obj"),
+        b.expressionStatement(
+            b.callExpression(b.identifier("log"), [b.identifier("key")])
+        ),
+        /* each: */ false
+    );
+
+    assert.strictEqual(
+        printer.print(loop).code,
+        "for (var key in obj)\n" +
+        "  log(key);"
+    );
 
     t.finish();
 };