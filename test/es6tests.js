f222d903853c81e80945ef888d2db2f545e15c02
Ben Newman <bn@cs.stanford.edu>
2014-10-05 16:47:33 -0400

    Subjectively improve formatting in test/es6tests.js.

--- test/es6tests.js
+++ test/es6tests.js
@@ -5,56 +5,61 @@ var n = require("../lib/types").namedTypes;
 var b = require("../lib/types").builders;
 
 describe("ES6 Compatability", function() {
-    it(
-        "correctly converts from a shorthand method to ES5 function",
-        function convertShorthandMethod() {
-            var printer = new Printer({ tabWidth: 2 });
-            var code = [
-                "var name='test-name';",
-                "var shorthandObj = {",
-                "  name,",
-                "  func() { return 'value'; }",
-                "};"
-            ].join("\n");
-            var ast = parse(code);
-            n.VariableDeclaration.assert(ast.program.body[1]);
-            var shorthandObjDec = ast.program.body[1].declarations[0].init;
-            var methodDecProperty = shorthandObjDec.properties[1];
-            var newES5MethodProperty = b.property(
-                methodDecProperty.kind,
-                methodDecProperty.key,
-                methodDecProperty.value,
-                false,
-                false
-            );
-            var correctMethodProperty = b.property(
-                methodDecProperty.kind,
-                methodDecProperty.key,
-                b.functionExpression(
-                    methodDecProperty.value.id,
-                    methodDecProperty.value.params,
-                    methodDecProperty.value.body,
-                    methodDecProperty.value.generator,
-                    methodDecProperty.value.expression
-                ),
-                false,
-                false
-            );
-            assert.strictEqual(
-                printer.print(newES5MethodProperty).code,
-                printer.print(correctMethodProperty).code
-            );
-        }
-    );
+    function convertShorthandMethod() {
+        var printer = new Printer({ tabWidth: 2 });
 
-  it(
-      "respects destructuring assignments",
-      function respectDestructuringAssignment() {
-          var printer = new Printer({ tabWidth: 2 });
-          var code = 'var {a} = {};';
-          var ast = parse(code);
-          n.VariableDeclaration.assert(ast.program.body[0]);
-          assert.strictEqual(printer.print(ast).code, code);
-      }
-  );
+        var code = [
+            "var name='test-name';",
+            "var shorthandObj = {",
+            "  name,",
+            "  func() { return 'value'; }",
+            "};"
+        ].join("\n");
+
+        var ast = parse(code);
+        n.VariableDeclaration.assert(ast.program.body[1]);
+
+        var shorthandObjDec = ast.program.body[1].declarations[0].init;
+        var methodDecProperty = shorthandObjDec.properties[1];
+        var newES5MethodProperty = b.property(
+            methodDecProperty.kind,
+            methodDecProperty.key,
+            methodDecProperty.value,
+            false,
+            false
+        );
+
+        var correctMethodProperty = b.property(
+            methodDecProperty.kind,
+            methodDecProperty.key,
+            b.functionExpression(
+                methodDecProperty.value.id,
+                methodDecProperty.value.params,
+                methodDecProperty.value.body,
+                methodDecProperty.value.generator,
+                methodDecProperty.value.expression
+            ),
+            false,
+            false
+        );
+
+        assert.strictEqual(
+            printer.print(newES5MethodProperty).code,
+            printer.print(correctMethodProperty).code
+        );
+    }
+
+    it("correctly converts from a shorthand method to ES5 function",
+       convertShorthandMethod);
+
+    function respectDestructuringAssignment() {
+        var printer = new Printer({ tabWidth: 2 });
+        var code = 'var {a} = {};';
+        var ast = parse(code);
+        n.VariableDeclaration.assert(ast.program.body[0]);
+        assert.strictEqual(printer.print(ast).code, code);
+    }
+
+    it("respects destructuring assignments",
+       respectDestructuringAssignment);
 });
