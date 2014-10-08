a270c0ce3c4130404fded339e9ebb67139c3f290
Ben Newman <bn@cs.stanford.edu>
2014-10-08 11:20:27 -0400

    Update printer for new ES6 module syntax.
    This involved upgrading ast-types to 0.5.0 and esprima-fb to
    7001.1.0-dev-harmony-fb as well. Since those changes to ast-types and
    esprima-fb were considered breaking changes, I think we have to consider
    this commit a breaking change as well.
    
    Closes #118.
    Closes #119.

--- test/es6tests.js
+++ test/es6tests.js
@@ -63,3 +63,214 @@ describe("ES6 Compatability", function() {
     it("respects destructuring assignments",
        respectDestructuringAssignment);
 });
+
+describe("import/export syntax", function() {
+    var recast = require("recast");
+
+    function check(source) {
+        var ast1 = recast.parse(source);
+        var ast2 = recast.parse(recast.prettyPrint(ast1).code);
+        recast.types.astNodesAreEquivalent.assert(ast1, ast2);
+    }
+
+    it("should parse and print import statements correctly", function() {
+        check("import foo from 'foo'");
+
+        // default imports
+        check("import foo from 'foo';");
+        check("import {default as foo} from 'foo';");
+
+        // named imports
+        check("import {bar} from 'foo';");
+        check("import {bar, baz} from 'foo';");
+        check("import {bar as baz} from 'foo';");
+        check("import {bar as baz, xyz} from 'foo';");
+
+        // glob imports
+        check("import * as foo from 'foo';");
+
+        // mixing imports
+        check("import foo, {baz as xyz} from 'foo';");
+        check("import foo, * as bar from 'foo';");
+
+        // just import
+        check("import 'foo';");
+    });
+
+    it("should parse and print export statements correctly", function() {
+        // default exports
+        check("export default 42;");
+        check("export default {};");
+        check("export default [];");
+        check("export default foo;");
+        check("export default function () {};");
+        check("export default class {};");
+        check("export default function foo () {};");
+        check("export default class foo {};");
+
+        // variables exports
+        check("export var foo = 1;");
+        check("export var foo = function () {};");
+        check("export var bar;"); // lazy initialization
+        check("export let foo = 2;");
+        check("export let bar;"); // lazy initialization
+        check("export const foo = 3;");
+        check("export function foo () {};");
+        check("export class foo {};");
+
+        // named exports
+        check("export {foo};");
+        check("export {foo, bar};");
+        check("export {foo as bar};");
+        check("export {foo as default};");
+        check("export {foo as default, bar};");
+
+        // exports from
+        check("export * from 'foo';");
+        check("export {foo} from 'foo';");
+        check("export {foo, bar} from 'foo';");
+        check("export {foo as bar} from 'foo';");
+        check("export {foo as default} from 'foo';");
+        check("export {foo as default, bar} from 'foo';");
+        check("export {default} from 'foo';");
+        check("export {default as foo} from 'foo';");
+    });
+
+    it("should forbid invalid import/export syntax", function() {
+        function checkInvalid(source, expectedMessage) {
+            try {
+                recast.parse(source);
+                throw new Error("Parsing should have failed: " +
+                                JSON.stringify(source));
+            } catch (err) {
+                assert.strictEqual(err.message, "Line 1: " + expectedMessage);
+            }
+        }
+
+        // const variables must have an initializer
+        checkInvalid(
+            "export const bar;",
+            "Const must be initialized"
+        );
+
+        // Unexpected token identifier, invalid named export syntax
+        checkInvalid(
+            "export foo;",
+            "Unexpected identifier"
+        );
+
+        // Unexpected token (, use a function declaration instead
+        checkInvalid(
+            "export function () {}",
+            "Unexpected token ("
+        );
+
+        // Unexpected token default
+        checkInvalid(
+            "export function default () {}",
+            "Unexpected token default"
+        );
+
+        // Missing from after import
+        checkInvalid(
+            "import foo;",
+            "Unexpected token ;"
+        );
+
+        // Missing from after import
+        checkInvalid(
+            "import { foo, bar };",
+            "Unexpected token ;"
+        );
+
+        // Invalid module specifier
+        checkInvalid(
+            "import foo from bar;",
+            "Invalid module specifier"
+        );
+
+        // Unexpected token default
+        checkInvalid(
+            "import default from 'foo';",
+            "Unexpected token default"
+        );
+
+        // Unexpected token from
+        checkInvalid(
+            "export default from 'foo';",
+            "Unexpected token from"
+        );
+
+        // Missing from after export
+        checkInvalid(
+            "export {default};",
+            "Unexpected token ;"
+        );
+
+        // Missing from after export
+        checkInvalid(
+            "export *;",
+            "Unexpected token ;"
+        );
+
+        // Missing from after import
+        checkInvalid(
+            "import {default as foo};",
+            "Unexpected token ;"
+        );
+
+        // Missing as after import *
+        checkInvalid(
+            "import * from 'foo';",
+            "Missing as after import *"
+        );
+
+        // Unexpected token =
+        checkInvalid(
+            "export default = 42;",
+            "Unexpected token ="
+        );
+
+        // Unexpected token default
+        checkInvalid(
+            "import {bar as default} from 'foo';",
+            "Unexpected token default"
+        );
+
+        // Unexpected token ,
+        checkInvalid(
+            "import foo, * as bar, {baz as xyz} from 'foo';",
+            "Unexpected token ,"
+        );
+
+        // Unexpected token ,
+        checkInvalid(
+            "import {bar}, foo from 'foo';",
+            "Unexpected token ,"
+        );
+
+        // Unexpected token ,
+        checkInvalid(
+            "import {bar}, * as foo from 'foo';",
+            "Unexpected token ,"
+        );
+
+        // Unexpected token ,
+        checkInvalid(
+            "import foo, {bar}, foo from 'foo';",
+            "Unexpected token ,"
+        );
+
+        // Unexpected token ,
+        checkInvalid(
+            "import {bar}, {foo} from 'foo';",
+            "Unexpected token ,"
+        );
+
+        // Unexpected token ,
+        checkInvalid(
+            "import * as bar, {baz as xyz} from 'foo';",
+            "Unexpected token ,"
+        );
+    });
+});
