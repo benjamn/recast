760d4572462600da35465d38ce474ab6c04149fa
Ben Newman <bn@cs.stanford.edu>
2015-11-10 17:55:13 -0500

    Reindent test/parser.js.
    I really can't stand four-space tabs anymore.

--- test/parser.js
+++ test/parser.js
@@ -15,134 +15,135 @@ var eol = require("os").EOL;
 // test functions with names and then export them later.
 
 describe("parser", function() {
-    it("Parser", function testParser(done) {
-        var code = testParser + "";
-        var ast = parse(code);
+  it("Parser", function testParser(done) {
+    var code = testParser + "";
+    var ast = parse(code);
 
-        namedTypes.File.assert(ast);
-        assert.ok(getReprinter(FastPath.from(ast)));
+    namedTypes.File.assert(ast);
+    assert.ok(getReprinter(FastPath.from(ast)));
 
-        var funDecl = ast.program.body[0],
-            funBody = funDecl.body;
+    var funDecl = ast.program.body[0];
+    var funBody = funDecl.body;
 
-        namedTypes.FunctionDeclaration.assert(funDecl);
-        namedTypes.BlockStatement.assert(funBody);
-        assert.ok(getReprinter(FastPath.from(funBody)));
+    namedTypes.FunctionDeclaration.assert(funDecl);
+    namedTypes.BlockStatement.assert(funBody);
+    assert.ok(getReprinter(FastPath.from(funBody)));
 
-        var lastStatement = funBody.body.pop(),
-            doneCall = lastStatement.expression;
+    var lastStatement = funBody.body.pop();
+    var doneCall = lastStatement.expression;
 
-        assert.ok(!getReprinter(FastPath.from(funBody)));
-        assert.ok(getReprinter(FastPath.from(ast)));
+    assert.ok(!getReprinter(FastPath.from(funBody)));
+    assert.ok(getReprinter(FastPath.from(ast)));
 
-        funBody.body.push(lastStatement);
-        assert.ok(getReprinter(FastPath.from(funBody)));
+    funBody.body.push(lastStatement);
+    assert.ok(getReprinter(FastPath.from(funBody)));
 
-        assert.strictEqual(doneCall.callee.name, "done");
+    assert.strictEqual(doneCall.callee.name, "done");
 
-        assert.strictEqual(lastStatement.comments.length, 2);
+    assert.strictEqual(lastStatement.comments.length, 2);
 
-        var firstComment = lastStatement.comments[0];
-        assert.strictEqual(firstComment.type, "Line");
-        assert.strictEqual(firstComment.leading, true);
-        assert.strictEqual(firstComment.trailing, false);
-        assert.strictEqual(
-            firstComment.value,
-            " Make sure done() remains the final statement in this function,"
-        );
+    var firstComment = lastStatement.comments[0];
+    assert.strictEqual(firstComment.type, "Line");
+    assert.strictEqual(firstComment.leading, true);
+    assert.strictEqual(firstComment.trailing, false);
+    assert.strictEqual(
+      firstComment.value,
+      " Make sure done() remains the final statement in this function,"
+    );
 
-        var secondComment = lastStatement.comments[1];
-        assert.strictEqual(secondComment.type, "Line");
-        assert.strictEqual(secondComment.leading, true);
-        assert.strictEqual(secondComment.trailing, false);
-        assert.strictEqual(
-            secondComment.value,
-            " or the above assertions will probably fail."
-        );
+    var secondComment = lastStatement.comments[1];
+    assert.strictEqual(secondComment.type, "Line");
+    assert.strictEqual(secondComment.leading, true);
+    assert.strictEqual(secondComment.trailing, false);
+    assert.strictEqual(
+      secondComment.value,
+      " or the above assertions will probably fail."
+    );
 
-        // Make sure done() remains the final statement in this function,
-        // or the above assertions will probably fail.
-        done();
+    // Make sure done() remains the final statement in this function,
+    // or the above assertions will probably fail.
+    done();
+  });
+
+  it("LocationFixer", function() {
+    var code = [
+      "function foo() {",
+      "    a()",
+      "    b()",
+      "}"
+    ].join(eol);
+    var ast = parse(code);
+    var printer = new Printer;
+
+    types.visit(ast, {
+      visitFunctionDeclaration: function(path) {
+        path.node.body.body.reverse();
+        this.traverse(path);
+      }
     });
 
-    it("LocationFixer", function() {
-        var code = [
-            "function foo() {",
-            "    a()",
-            "    b()",
-            "}"
-        ].join(eol);
-        var ast = parse(code);
-        var printer = new Printer;
+    var altered = code
+      .replace("a()", "xxx")
+      .replace("b()", "a()")
+      .replace("xxx", "b()");
 
-        types.visit(ast, {
-            visitFunctionDeclaration: function(path) {
-                path.node.body.body.reverse();
-                this.traverse(path);
-            }
-        });
+    assert.strictEqual(altered, printer.print(ast).code);
+  });
 
-        var altered = code
-            .replace("a()", "xxx")
-            .replace("b()", "a()")
-            .replace("xxx", "b()");
+  it("TabHandling", function() {
+    function check(code, tabWidth) {
+      var lines = fromString(code, { tabWidth: tabWidth });
+      assert.strictEqual(lines.length, 1);
 
-        assert.strictEqual(altered, printer.print(ast).code);
-    });
+      types.visit(parse(code, { tabWidth: tabWidth }), {
+        check: function(s, loc) {
+          var sliced = lines.slice(loc.start, loc.end);
+          assert.strictEqual(s + "", sliced.toString());
+        },
 
-    it("TabHandling", function() {
-        function check(code, tabWidth) {
-            var lines = fromString(code, { tabWidth: tabWidth });
-            assert.strictEqual(lines.length, 1);
+        visitIdentifier: function(path) {
+          var ident = path.node;
+          this.check(ident.name, ident.loc);
+          this.traverse(path);
+        },
 
-            types.visit(parse(code, { tabWidth: tabWidth }), {
-                check: function(s, loc) {
-                    var sliced = lines.slice(loc.start, loc.end);
-                    assert.strictEqual(s + "", sliced.toString());
-                },
-
-                visitIdentifier: function(path) {
-                    var ident = path.node;
-                    this.check(ident.name, ident.loc);
-                    this.traverse(path);
-                },
-
-                visitLiteral: function(path) {
-                    var lit = path.node;
-                    this.check(lit.value, lit.loc);
-                    this.traverse(path);
-                }
-            });
+        visitLiteral: function(path) {
+          var lit = path.node;
+          this.check(lit.value, lit.loc);
+          this.traverse(path);
         }
+      });
+    }
 
-        for (var tabWidth = 1; tabWidth <= 8; ++tabWidth) {
-            check("\t\ti = 10;", tabWidth);
-            check("\t\ti \t= 10;", tabWidth);
-            check("\t\ti \t=\t 10;", tabWidth);
-            check("\t \ti \t=\t 10;", tabWidth);
-            check("\t \ti \t=\t 10;\t", tabWidth);
-            check("\t \ti \t=\t 10;\t ", tabWidth);
-        }
-    });
+    for (var tabWidth = 1; tabWidth <= 8; ++tabWidth) {
+      check("\t\ti = 10;", tabWidth);
+      check("\t\ti \t= 10;", tabWidth);
+      check("\t\ti \t=\t 10;", tabWidth);
+      check("\t \ti \t=\t 10;", tabWidth);
+      check("\t \ti \t=\t 10;\t", tabWidth);
+      check("\t \ti \t=\t 10;\t ", tabWidth);
+    }
+  });
 
-    it("AlternateEsprima", function() {
-        var types = require("../lib/types");
-        var b = types.builders;
-        var esprima = {
-            parse: function(code) {
-                var program = b.program([
-                    b.expressionStatement(b.identifier("surprise"))
-                ]);
-                program.comments = [];
-                return program;
-            }
-        };
-        var ast = parse("ignored", { esprima: esprima });
-        var printer = new Printer;
+  it("AlternateEsprima", function() {
+    var types = require("../lib/types");
+    var b = types.builders;
+    var esprima = {
+      parse: function(code) {
+        var program = b.program([
+          b.expressionStatement(b.identifier("surprise"))
+        ]);
+        program.comments = [];
+        return program;
+      }
+    };
+    var ast = parse("ignored", { esprima: esprima });
+    var printer = new Printer;
 
-        types.namedTypes.File.assert(ast, true);
-        assert.strictEqual(
-            printer.printGenerically(ast).code,
-            "surprise;");
-    });
+    types.namedTypes.File.assert(ast, true);
+    assert.strictEqual(
+      printer.printGenerically(ast).code,
+      "surprise;"
+    );
+  });
 });
