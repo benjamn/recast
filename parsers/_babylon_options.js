0d21decf2557465a40d5f0f31a22adb4cb80576e
Ben Newman <ben@benjamn.com>
2018-09-09 11:55:51 -0400

    Make sure all the preconfigured parsers expose ast.tokens.

--- parsers/_babylon_options.js
+++ parsers/_babylon_options.js
@@ -11,7 +11,7 @@ module.exports = function (options) {
     allowImportExportEverywhere: true,
     allowReturnOutsideFunction: true,
     startLine: 1,
-    tokens: getOption(options, "tokens", true),
+    tokens: true,
     plugins: [
       "asyncGenerators",
       "bigInt",
