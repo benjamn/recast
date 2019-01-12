8498bbb5b446b3ceea2e293d1b6730d4d167d652
Ben Newman <ben@benjamn.com>
2019-01-12 17:37:13 -0500

    Convert Mapping to a class.

--- lib/mapping.ts
+++ lib/mapping.ts
@@ -1,149 +1,162 @@
 import assert from "assert";
-import types from "./types";
-var isNumber = types.builtInTypes.number;
-var SourceLocation = types.namedTypes.SourceLocation;
-var Position = types.namedTypes.Position;
-import * as linesModule from "./lines";
 import { comparePos } from "./util";
+import {
+  SourceLocation as Loc,
+  Position as Pos,
+} from "./types";
+import { Lines } from "./lines";
 
-interface MappingType {
-  sourceLines: any;
-  sourceLoc: any;
-  targetLoc: any;
-  slice(lines: any, start: any, end: any): MappingType | null;
-  add(line: number, column: number): MappingType;
-  subtract(line: number, column: number): MappingType;
-  indent(by: any, skipFirstLine: any, noNegativeColumns: any): MappingType;
+export default class Mapping {
+  constructor(
+    public sourceLines: Lines,
+    public sourceLoc: Loc,
+    public targetLoc: Loc = sourceLoc,
+  ) {}
+
+  slice(
+    lines: Lines,
+    start: Pos,
+    end: Pos = lines.lastPos(),
+  ) {
+    var sourceLines = this.sourceLines;
+    var sourceLoc = this.sourceLoc;
+    var targetLoc = this.targetLoc;
+
+    function skip(name: "start" | "end") {
+      var sourceFromPos = sourceLoc[name];
+      var targetFromPos = targetLoc[name];
+      var targetToPos = start;
+
+      if (name === "end") {
+        targetToPos = end;
+      } else {
+        assert.strictEqual(name, "start");
+      }
+
+      return skipChars(
+        sourceLines, sourceFromPos,
+        lines, targetFromPos, targetToPos
+      );
+    }
+
+    if (comparePos(start, targetLoc.start) <= 0) {
+      if (comparePos(targetLoc.end, end) <= 0) {
+        targetLoc = {
+          start: subtractPos(targetLoc.start, start.line, start.column),
+          end: subtractPos(targetLoc.end, start.line, start.column)
+        };
+
+        // The sourceLoc can stay the same because the contents of the
+        // targetLoc have not changed.
+
+      } else if (comparePos(end, targetLoc.start) <= 0) {
+        return null;
+
+      } else {
+        sourceLoc = {
+          start: sourceLoc.start,
+          end: skip("end")
+        };
+
+        targetLoc = {
+          start: subtractPos(targetLoc.start, start.line, start.column),
+          end: subtractPos(end, start.line, start.column)
+        };
+      }
+
+    } else {
+      if (comparePos(targetLoc.end, start) <= 0) {
+        return null;
+      }
+
+      if (comparePos(targetLoc.end, end) <= 0) {
+        sourceLoc = {
+          start: skip("start"),
+          end: sourceLoc.end
+        };
+
+        targetLoc = {
+          // Same as subtractPos(start, start.line, start.column):
+          start: { line: 1, column: 0 },
+          end: subtractPos(targetLoc.end, start.line, start.column)
+        };
+
+      } else {
+        sourceLoc = {
+          start: skip("start"),
+          end: skip("end")
+        };
+
+        targetLoc = {
+          // Same as subtractPos(start, start.line, start.column):
+          start: { line: 1, column: 0 },
+          end: subtractPos(end, start.line, start.column)
+        };
+      }
+    }
+
+    return new Mapping(this.sourceLines, sourceLoc, targetLoc);
+  }
+
+  add(line: number, column: number) {
+    return new Mapping(this.sourceLines, this.sourceLoc, {
+      start: addPos(this.targetLoc.start, line, column),
+      end: addPos(this.targetLoc.end, line, column)
+    });
+  }
+
+  subtract(line: number, column: number) {
+    return new Mapping(this.sourceLines, this.sourceLoc, {
+      start: subtractPos(this.targetLoc.start, line, column),
+      end: subtractPos(this.targetLoc.end, line, column)
+    });
+  }
+
+  indent(
+    by: number,
+    skipFirstLine: boolean = false,
+    noNegativeColumns: boolean = false,
+  ) {
+    if (by === 0) {
+      return this;
+    }
+
+    var targetLoc = this.targetLoc;
+    var startLine = targetLoc.start.line;
+    var endLine = targetLoc.end.line;
+
+    if (skipFirstLine && startLine === 1 && endLine === 1) {
+      return this;
+    }
+
+    targetLoc = {
+      start: targetLoc.start,
+      end: targetLoc.end
+    };
+
+    if (!skipFirstLine || startLine > 1) {
+      var startColumn = targetLoc.start.column + by;
+      targetLoc.start = {
+        line: startLine,
+        column: noNegativeColumns
+          ? Math.max(0, startColumn)
+          : startColumn
+      };
+    }
+
+    if (!skipFirstLine || endLine > 1) {
+      var endColumn = targetLoc.end.column + by;
+      targetLoc.end = {
+        line: endLine,
+        column: noNegativeColumns
+          ? Math.max(0, endColumn)
+          : endColumn
+      };
+    }
+
+    return new Mapping(this.sourceLines, this.sourceLoc, targetLoc);
+  }
 }
-
-interface MappingConstructor {
-  new(sourceLines: any, sourceLoc: any, targetLoc?: any): MappingType;
-}
-
-const Mapping = function Mapping(this: MappingType, sourceLines: any, sourceLoc: any, targetLoc?: any) {
-  assert.ok(this instanceof Mapping);
-  assert.ok(sourceLines instanceof linesModule.Lines);
-  SourceLocation.assert(sourceLoc);
-
-  if (targetLoc) {
-    // In certain cases it's possible for targetLoc.{start,end}.column
-    // values to be negative, which technically makes them no longer
-    // valid SourceLocation nodes, so we need to be more forgiving.
-    assert.ok(isNumber.check(targetLoc.start.line) &&
-              isNumber.check(targetLoc.start.column) &&
-              isNumber.check(targetLoc.end.line) &&
-              isNumber.check(targetLoc.end.column));
-  } else {
-    // Assume identity mapping if no targetLoc specified.
-    targetLoc = sourceLoc;
-  }
-
-  Object.defineProperties(this, {
-    sourceLines: { value: sourceLines },
-    sourceLoc: { value: sourceLoc },
-    targetLoc: { value: targetLoc }
-  });
-} as any as MappingConstructor;
-
-var Mp: MappingType = Mapping.prototype;
-export default Mapping;
-
-Mp.slice = function(lines, start, end) {
-  assert.ok(lines instanceof linesModule.Lines);
-  Position.assert(start);
-
-  if (end) {
-    Position.assert(end);
-  } else {
-    end = lines.lastPos();
-  }
-
-  var sourceLines = this.sourceLines;
-  var sourceLoc = this.sourceLoc;
-  var targetLoc = this.targetLoc;
-
-  function skip(name: string) {
-    var sourceFromPos = sourceLoc[name];
-    var targetFromPos = targetLoc[name];
-    var targetToPos = start;
-
-    if (name === "end") {
-      targetToPos = end;
-    } else {
-      assert.strictEqual(name, "start");
-    }
-
-    return skipChars(
-      sourceLines, sourceFromPos,
-      lines, targetFromPos, targetToPos
-    );
-  }
-
-  if (comparePos(start, targetLoc.start) <= 0) {
-    if (comparePos(targetLoc.end, end) <= 0) {
-      targetLoc = {
-        start: subtractPos(targetLoc.start, start.line, start.column),
-        end: subtractPos(targetLoc.end, start.line, start.column)
-      };
-
-      // The sourceLoc can stay the same because the contents of the
-      // targetLoc have not changed.
-
-    } else if (comparePos(end, targetLoc.start) <= 0) {
-      return null;
-
-    } else {
-      sourceLoc = {
-        start: sourceLoc.start,
-        end: skip("end")
-      };
-
-      targetLoc = {
-        start: subtractPos(targetLoc.start, start.line, start.column),
-        end: subtractPos(end, start.line, start.column)
-      };
-    }
-
-  } else {
-    if (comparePos(targetLoc.end, start) <= 0) {
-      return null;
-    }
-
-    if (comparePos(targetLoc.end, end) <= 0) {
-      sourceLoc = {
-        start: skip("start"),
-        end: sourceLoc.end
-      };
-
-      targetLoc = {
-        // Same as subtractPos(start, start.line, start.column):
-        start: { line: 1, column: 0 },
-        end: subtractPos(targetLoc.end, start.line, start.column)
-      };
-
-    } else {
-      sourceLoc = {
-        start: skip("start"),
-        end: skip("end")
-      };
-
-      targetLoc = {
-        // Same as subtractPos(start, start.line, start.column):
-        start: { line: 1, column: 0 },
-        end: subtractPos(end, start.line, start.column)
-      };
-    }
-  }
-
-  return new Mapping(this.sourceLines, sourceLoc, targetLoc);
-};
-
-Mp.add = function(line, column) {
-  return new Mapping(this.sourceLines, this.sourceLoc, {
-    start: addPos(this.targetLoc.start, line, column),
-    end: addPos(this.targetLoc.end, line, column)
-  });
-};
 
 function addPos(toPos: any, line: number, column: number) {
   return {
@@ -154,13 +167,6 @@ function addPos(toPos: any, line: number, column: number) {
   };
 }
 
-Mp.subtract = function(line, column) {
-  return new Mapping(this.sourceLines, this.sourceLoc, {
-    start: subtractPos(this.targetLoc.start, line, column),
-    end: subtractPos(this.targetLoc.end, line, column)
-  });
-};
-
 function subtractPos(fromPos: any, line: number, column: number) {
   return {
     line: fromPos.line - line + 1,
@@ -170,57 +176,13 @@ function subtractPos(fromPos: any, line: number, column: number) {
   };
 }
 
-Mp.indent = function(by, skipFirstLine, noNegativeColumns) {
-  if (by === 0) {
-    return this;
-  }
-
-  var targetLoc = this.targetLoc;
-  var startLine = targetLoc.start.line;
-  var endLine = targetLoc.end.line;
-
-  if (skipFirstLine && startLine === 1 && endLine === 1) {
-    return this;
-  }
-
-  targetLoc = {
-    start: targetLoc.start,
-    end: targetLoc.end
-  };
-
-  if (!skipFirstLine || startLine > 1) {
-    var startColumn = targetLoc.start.column + by;
-    targetLoc.start = {
-      line: startLine,
-      column: noNegativeColumns
-        ? Math.max(0, startColumn)
-        : startColumn
-    };
-  }
-
-  if (!skipFirstLine || endLine > 1) {
-    var endColumn = targetLoc.end.column + by;
-    targetLoc.end = {
-      line: endLine,
-      column: noNegativeColumns
-        ? Math.max(0, endColumn)
-        : endColumn
-    };
-  }
-
-  return new Mapping(this.sourceLines, this.sourceLoc, targetLoc);
-};
-
 function skipChars(
-  sourceLines: any, sourceFromPos: any,
-  targetLines: any, targetFromPos: any, targetToPos: any
+  sourceLines: Lines,
+  sourceFromPos: Pos,
+  targetLines: Lines,
+  targetFromPos: Pos,
+  targetToPos: Pos,
 ) {
-  assert.ok(sourceLines instanceof linesModule.Lines);
-  assert.ok(targetLines instanceof linesModule.Lines);
-  Position.assert(sourceFromPos);
-  Position.assert(targetFromPos);
-  Position.assert(targetToPos);
-
   var targetComparison = comparePos(targetFromPos, targetToPos);
   if (targetComparison === 0) {
     // Trivial case: no characters to skip.
@@ -229,9 +191,8 @@ function skipChars(
 
   if (targetComparison < 0) {
     // Skipping forward.
-
-    var sourceCursor = sourceLines.skipSpaces(sourceFromPos);
-    var targetCursor = targetLines.skipSpaces(targetFromPos);
+    var sourceCursor = sourceLines.skipSpaces(sourceFromPos) || sourceLines.lastPos();
+    var targetCursor = targetLines.skipSpaces(targetFromPos) || targetLines.lastPos();
 
     var lineDiff = targetToPos.line - targetCursor.line;
     sourceCursor.line += lineDiff;
@@ -257,9 +218,8 @@ function skipChars(
 
   } else {
     // Skipping backward.
-
-    var sourceCursor = sourceLines.skipSpaces(sourceFromPos, true);
-    var targetCursor = targetLines.skipSpaces(targetFromPos, true);
+    var sourceCursor = sourceLines.skipSpaces(sourceFromPos, true) || sourceLines.firstPos();
+    var targetCursor = targetLines.skipSpaces(targetFromPos, true) || targetLines.firstPos();
 
     var lineDiff = targetToPos.line - targetCursor.line;
     sourceCursor.line += lineDiff;
