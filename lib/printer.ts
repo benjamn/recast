036011539edcef671f8099899aa65b333eb3acc6
Ben Newman <ben@benjamn.com>
2019-03-19 10:44:02 -0400

    Simplify inexact object type printing.

--- lib/printer.ts
+++ lib/printer.ts
@@ -715,20 +715,18 @@ function genericPrintNoParens(path: any, options: any, print: any) {
         });
 
         if (n.inexact) {
-          var line = fromString("...", options);
-          if (!oneLine) {
-            parts.push("\n");
-            line = line.indent(options.tabWidth);
-            parts.push(line);
-            // No trailing separator after ... to maintain parity with prettier
-          } else {
-            if (len > 0) {
-              parts.push(separator);
-              parts.push(" ");
+            const line = fromString("...", options);
+            if (oneLine) {
+                if (len > 0) {
+                    parts.push(separator, " ");
+                }
+                parts.push(line);
+            } else {
+                // No trailing separator after ... to maintain parity with prettier.
+                parts.push("\n", line.indent(options.tabWidth));
             }
-            parts.push(line);
-          }
         }
+
         parts.push(oneLine ? rightBrace : "\n" + rightBrace);
 
         if (i !== 0 && oneLine && options.objectCurlySpacing) {
