2e0072d442ee0533e0f2c33ea70ecd7daa7150f1
Bogdan Savluk <savluk.bogdan@gmail.com>
2020-12-16 21:27:16 +0100

    update the test

--- test/printer.ts
+++ test/printer.ts
@@ -1135,15 +1135,23 @@ describe("printer", function () {
     const pretty = printer.printGenerically(ast).code;
     assert.strictEqual(pretty, code);
   });
-  
+
   it("adds parenthesis around arrow functions body when returning object expression using babel parser", function () {
-    const expected = "() => ({a: 'b'});";
-    const source = "(a) => ({a: 'b'});";
+    const expected = [
+      "() => ({",
+      "  a: 'b'",
+      "});",
+    ].join(eol);
+    const source = [
+      "(a) => ({",
+      "  a: 'b'",
+      "});"
+    ].join(eol);
     const ast = recast.parse(source, {
         parser: require('@babel/parser'),
     });
     const traverse = require('@babel/traverse').default;
-    
+
     traverse(ast, {
         Function(path: any) {
             path.get('params.0').remove();
