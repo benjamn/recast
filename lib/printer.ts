bc030e55305795698fd7d80a6df4d9cb38fd14f8
Dennis Kuduzovic <dkuduzovic@auditboard.com>
2022-07-19 21:34:32 -0600

    add override keyword to printMethod function

--- lib/printer.ts
+++ lib/printer.ts
@@ -2741,6 +2741,10 @@ function printMethod(path: any, options: any, print: any) {
     parts.push("abstract ");
   }
 
+  if (node.override) {
+    parts.push("override ");
+  }
+
   if (node.readonly) {
     parts.push("readonly ");
   }
